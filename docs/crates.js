5765de40a1f4291f15bb1b547ce6811a3f8fbfdd
Jcharis <jcharistech@gmail.com>
2025-01-09 17:40:39 +0100

    chore: add api docs

---
+++ docs/crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["cipha_cli","cipha_lib"];
+//{"start":21,"fragment_lengths":[11,12]}
---
+++ docs/src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["cipha_cli",["",[],["main.rs"]]],["cipha_lib",["",[],["ciphers.rs","lib.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[33,46]}
