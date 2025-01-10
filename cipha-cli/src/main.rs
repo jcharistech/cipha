9b405009396f5e73ad84034c66ac5a2db52ed901
Jcharis <jcharistech@gmail.com>
2025-01-10 12:25:05 +0100

    chore: modify docs

--- cipha-cli/src/main.rs
+++ cipha-cli/src/main.rs
@@ -1,10 +1,21 @@
+//! # cipha-cli
+//!
+//! `cipha-cli` is a CLI that supports various classifical ciphers such as ROT13, Caesar, Vigenere, Gematria and more.
+//! It is an interface to the `cipha` library or crate.
+//! ### Usage
+//! ```bash
+//! cipha-cli encode --cipher rot13 --message "Hello, World!"
+//! cipha-cli encode --cipher caesar --message "Hello, World!" --shift 3
+//! ```
+//!
+
 use structopt::StructOpt;
 use std::fs::File;
 use std::io::{Write, Read, stdout};
 extern crate cipha; 
 use cipha::utils::{alpha2num, atbash_cipher, atbash_decipher, caesar_cipher, morse_code_cipher, morse_code_decipher, num2alpha, reverse_cipher, rot13, vigenere_cipher, vigenere_decipher};
 
-/// A simple CLI for ciphers and crypto.
+/// A simple CLI for ciphers and cryptography.
 ///
 /// This CLI supports various ciphers such as ROT13, Caesar, Vigenere, and more.
 /// It can encode or decode messages using these ciphers.
